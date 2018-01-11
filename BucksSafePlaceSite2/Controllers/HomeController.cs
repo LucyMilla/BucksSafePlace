fdfe771fa338b37c699fe14e45fae697123eeaf0
jobelle <jobellevillamer@yahoo.com>
2018-01-11 16:38:29 +0000

    System Training page added
    Incidents, Locations & System Training pages updated. Only logged in users can access the pages
    
    Login page updated (Not "Hello user" to "Hello" + the user's email)

--- BucksSafePlaceSite2/Controllers/HomeController.cs
+++ BucksSafePlaceSite2/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace BucksSafePlaceSite2.Controllers
 {
-    [Authorize]
+    
     public class HomeController : Controller
     {
        
@@ -14,27 +14,19 @@ namespace BucksSafePlaceSite2.Controllers
         {
             return View();
         }
-
-        
+       
         public ActionResult About()
         {
-            
-
-            return View();
-        }
-        [Authorize(Roles = "Admin")]
-        public ActionResult Contact()
-        {
-            
-
             return View();
         }
         
-        [Authorize(Roles = "Admin")]
+        public ActionResult Contact()
+        {          
+            return View();
+        }
+              
         public ActionResult Medical()
         {
-           
-
             return View();
         }
 
@@ -42,22 +34,21 @@ namespace BucksSafePlaceSite2.Controllers
         public ActionResult SystemTraining()
         {
             return View();
-        }
-        [Authorize(Roles = "Admin")]
-        public ActionResult Roles()
-        {
-            return View();
-        }
+        }       
 
         [Authorize(Roles = "Admin")]
         public ActionResult Incidents()
         {
             return View();
         }
+
         [Authorize(Roles = "Admin")]
         public ActionResult Locations()
         {
             return View();
         }
+       
+       
+        
     }
 }
